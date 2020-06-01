0fe767f2680e7af09a39ad7be41afcde457c9a31
bee <bernhard.spindler.75@gmail.com>
2020-06-01 07:43:58 +0200

    initial DotNetCore

---
+++ UDPRacer_DotNetCore/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDPRacer
+{
+    class Program
+    {
+        public static long _GLOBAL_packages;
+
+        /// <summary>
+        /// race for speed and fun
+        /// </summary>
+        /// <param name="IPs">IPs of the host. seperated by comma</param>
+        /// <param name="inject">injet package and exit</param>
+        /// <param name="port">UDP port to listen (default: 4444)</param>
+        static void Main(string IPs, bool inject = false, int port = 4444)
+        {
+            if ( inject )
+            {
+                var hosts = IPs.Split(",").Select(ip => IPAddress.Parse(ip)).ToList();
+
+                byte[] initialPacket = Race.CreateNetworkPackage(hosts);
+                using (UdpClient udpc = new UdpClient())
+                {
+                    var firstHost = new IPEndPoint(hosts[0], port);
+                    udpc.Send(initialPacket, initialPacket.Length, firstHost);
+                    Console.WriteLine($"injected package to {firstHost}");
+                }
+                return;
+            }
+
+            Race.Start(port);
+
+            ManualResetEvent ended = new ManualResetEvent(false);
+            while (!ended.WaitOne(1000))
+            {
+                long pkgPerSec = Interlocked.Exchange(ref _GLOBAL_packages, 0);
+                Console.WriteLine($"{pkgPerSec}/s");
+            }
+        }
+    }
+}
---
+++ UDPRacer_DotNetCore/Race.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPRacer
+{
+    class Race
+    {
+        /// <summary>
+        /// typedef struct _DATA {
+        ///     DWORD Id;
+        ///     DWORD Hops;
+        ///     DWORD Next;
+        ///     DWORD Last;
+        ///     SOCKADDR_IN NextAddrIn[NUMBER_OF_MAX_CHAIN];
+        /// }
+        /// DATA;
+        /// </summary>
+        /// <param name="port"></param>
+        public static async void Start(int port)
+        {
+            UdpClient udpSock = new UdpClient(port);
+            IPEndPoint nextNode = new IPEndPoint(0, port);
+
+            while (true)
+            {
+                UdpReceiveResult data = await udpSock.ReceiveAsync().ConfigureAwait(false);
+                Interlocked.Increment(ref Program._GLOBAL_packages);
+                long nextIP;
+
+                unsafe
+                {
+                    fixed (byte* bptr = &data.Buffer[0])
+                    {
+                        UInt32* hops = (UInt32*)bptr[4];
+                        UInt32* next = (UInt32*)bptr[8];
+                        UInt32* last = (UInt32*)bptr[12];
+
+                        ++(*hops);
+                        *next = (*next + 1) % *last;
+
+                        long* ips = (long*)(bptr + 16);
+                        nextIP = ips[*next];
+                    }
+                }
+
+                nextNode.Address = new IPAddress(nextIP);
+                udpSock.SendAsync(data.Buffer, data.Buffer.Length, nextNode).ConfigureAwait(false);
+            }
+        }
+
+        public static byte[] CreateNetworkPackage(IReadOnlyList<IPAddress> IPs)
+        {
+            byte[] packet;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryWriter bw = new BinaryWriter(ms);
+
+                bw.Write((UInt32)0x6619FEAF);      // id
+                bw.Write((UInt32)0);               // hops
+                bw.Write((UInt32)0);               // next
+                bw.Write((UInt32)IPs.Count-1);     // last
+
+                foreach (IPAddress ip in IPs)
+                {
+                    bw.Write(ip.GetAddressBytes());
+                }
+
+                packet = ms.ToArray();
+            }
+
+            return packet;
+        }
+
+    }
+}
